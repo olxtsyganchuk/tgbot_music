9f27d0656a9d191506437fd86f47be8dfb081944
olxtsyganchuk <alxtsyganchuk@gmail.com>
2026-09-20 02:22:04 +0300

    Додано config.py до репозиторія, прибрано ігнорування

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+# Зчитуємо токен з Environment Variables на сервері або з .env локально
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+# Інші налаштування вебхука (їх можна залишити тут або теж винести в середовище)
+TARGET_PLATFORM = "youtubeMusic"
+WEB_SERVER_HOST = "0.0.0.0"       # Для сервера важливо слухати всі інтерфейси
+WEB_SERVER_PORT = int(os.getenv("PORT", 8080)) # Render часто передає порт динамічно через змінну PORT
+WEBHOOK_PATH = "/webhook"
