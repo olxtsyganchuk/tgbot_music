c229cc1e28aaa0987494cf6fbe2c0911d69fc8ad
olxtsyganchuk <alxtsyganchuk@gmail.com>
2026-09-20 02:32:43 +0300

    Додано зчитування BASE_WEBHOOK_URL в config

--- config.py
+++ config.py
@@ -8,3 +8,6 @@ TARGET_PLATFORM = "youtubeMusic"
 WEB_SERVER_HOST = "0.0.0.0"       # Для сервера важливо слухати всі інтерфейси
 WEB_SERVER_PORT = int(os.getenv("PORT", 8080)) # Render часто передає порт динамічно через змінну PORT
 WEBHOOK_PATH = "/webhook"
+
+BASE_WEBHOOK_URL = os.getenv("BASE_WEBHOOK_URL")
+WEBHOOK_SECRET = os.getenv("WEBHOOK_SECRET", None)
